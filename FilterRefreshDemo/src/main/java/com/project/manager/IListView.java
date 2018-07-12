9ad37cf61250e2c2e6cbbc835570b6c8e1b5e928
zzw <840631861@qq.com>
2018-07-12 16:13:55 +0800

    test

---
+++ FilterRefreshDemo/src/main/java/com/project/manager/IListView.java
@@ -0,0 +1,32 @@
+package com.project.manager;
+
+import com.project.model.FilterData;
+
+/**
+ * 有关接口.
+ * Created by Administrator on 2018/6/23.
+ */
+
+public interface IListView
+{
+    //排序按钮点击事件
+    interface OnSortClickListener{
+        void onSortClick(int status);
+    }
+    //综合下拉菜单选中事件
+    interface OnComSpinnerSelectedListener{
+        void onComSpinnerSelected(int index);
+    }
+    //筛选弹窗中确认点击事件
+    interface OnFilterConfirmClickListener{
+        void onFilterConfirmClick(FilterData data);
+    }
+    //筛选弹窗中重置按钮事件
+    interface OnFilterResetClickListener{
+        void onFilterResetClick(FilterData data);
+    }
+    //筛选弹窗中item状态改变或数值变动时监听
+    interface OnFilterItemChangeListener{
+        void onFilterItemChange(FilterData data);
+    }
+}
